5694c99eeddc57c4dff251d50849cc6f62d6cb9e
Dev <Dev@DESKTOP-2E8NSE8>
2021-07-20 20:46:46 +0530

    C

---
+++ SimpleMavenDemoProject/src/main/java/com/qa/test/TestngTestpkg.java
@@ -0,0 +1,16 @@
+package com.qa.test;
+
+import org.testng.annotations.Test;
+
+public class TestngTestpkg {
+
+	
+	@Test
+	
+	public void Test001() {
+		
+		System.out.println("TEST");
+		
+	}
+	
+}
---
+++ SimpleMavenDemoProject/src/test/java/sampleDemoTest.java
@@ -0,0 +1,13 @@
+import org.testng.annotations.Test;
+
+public class sampleDemoTest {
+
+	
+	@Test
+	public void test001() {
+		
+		System.out.println("TESTNG");
+	}
+	
+	
+}
